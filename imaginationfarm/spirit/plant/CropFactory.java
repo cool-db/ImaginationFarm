6e580d7cf3d9838c7643858b542611d2d3cf0efb
Simon Gavins Lee <simongavinslee0013@icloud.com>
2017-10-29 19:05:19 +0800

    First commit Flyweight Pattern

---
+++ imaginationfarm/spirit/plant/CropFactory.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+
+public class CropFactory {
+    private static final HashMap<String, Crop> wheatMap = new HashMap<>();
+
+    public static Crop getWheat(String strain) {
+        Wheat wheat = (Wheat) wheatMap.get(strain);
+
+        if(wheat == null) {
+            wheat = new Wheat(strain);
+            wheatMap.put(strain, wheat);
+            System.out.println("Planting wheat of strain : " + strain);
+        }
+        return wheat;
+    }
+}
---
+++ imaginationfarm/spirit/plant/Wheat.java
@@ -0,0 +1,22 @@
+public class Wheat implements Crop {
+    private String strain;
+    private double surviveRate;
+    private int height;
+
+    public Wheat(String strain){
+        this.strain = strain;
+    }
+
+    public void setSurviveRate(double surviveRate) {
+        this.surviveRate = surviveRate;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    @Override
+    public void plant() {
+        System.out.println("Wheat: Plant() [Strain : " + strain + ", Survive Rate : " + surviveRate +", Height :" + height);
+    }
+}
